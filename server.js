d9e5993adedf742c929d8b55985de3452c720e4d
oscar <oscaroceguera@gmail.com>
2015-08-29 01:50:26 -0600

    Introduccion a io.js
    Conceptos basicos y primer servidor

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const http = require('http');
+const port = process.env.PORT || 8080;
+
+const server = http.createServer();
+
+server.on('request', onRequest);
+server.on('listening', onListening)
+
+server.listen(port)
+
+function onRequest(req, res){
+	res.end('hola io.js');
+}
+
+function onListening(){
+	console.log("servidor en escuchando en el puerto : " + port);
+}
