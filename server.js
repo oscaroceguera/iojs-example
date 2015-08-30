392d30f240931416ed2ada7840210e8199cede1c
oscar <oscaroceguera@gmail.com>
2015-08-29 21:39:27 -0600

    Usar path

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 'use strict'
 
 const http = require('http');
-const fs = require('fs');
+const fs = require('fs'); // Cargar archivo
+const path = require('path'); // Manejo de rutas de archivos
 const port = process.env.PORT || 8080;
 
 const server = http.createServer();
@@ -12,8 +13,15 @@ server.on('listening', onListening)
 server.listen(port)
 
 function onRequest(req, res){
+	/**
+	 * Crear rutas de un archivo sin usar strings
+	 * "path.join" : permite concatenar rutas y directorios
+	 * "__dirname" : directorio actual
+	 */
+	let fileName = path.join(__dirname, 'public', 'index.html');
+
 	// Cargar archivo de manera asincrona
-	fs.readFile('public/index.html', function(err, file){
+	fs.readFile(fileName, function(err, file){
 		if(err){
 			return res.end(err.message);
 		}
