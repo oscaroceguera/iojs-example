ad6f05507ceac4f1469d7f4f1cc08c1ffe66e463
oscar <oscaroceguera@gmail.com>
2015-08-30 01:03:14 -0600

    Rutas y manejo de imagenes

--- client/app.js
+++ client/app.js
@@ -1,3 +1,4 @@
+const xhr = require('xhr');
 const Webrtc2Images = require('webrtc2images');
 
 const rtc = new Webrtc2Images({
@@ -21,6 +22,16 @@ record.addEventListener('click', function(e){
 	// grabar video
 	rtc.recordVideo(function(err, frames){
 		if(err) return logError(err)
-		console.log(frames)
+		
+		xhr({
+			uri : '/process',
+			method : 'post',
+			headers : {'Content-Type' : 'application/json'},
+			body : JSON.stringify({images:frames}),
+		}, function(err,res,body){
+			if(err) return logError(err)
+
+			console.log(JSON.parse(body))
+		})
 	})
 }, false);
--- router/index.js
+++ router/index.js
@@ -1,6 +1,7 @@
 const st = require('st'); // servidor estatico
 const path = require('path');
 const course = require('course'); // rutas estaticas
+const jsonBody = require('body/json');
 
 const router = course();
 
@@ -8,6 +9,14 @@ const mount = st({
 	path : path.join(__dirname, '..', 'public'),
 	index : 'index.html',
 	passthrough : true
+});
+
+router.post('/process', function(req, res){
+	jsonBody(req,res,{ limit : 3 * 1024 * 1024 },function(err,body){
+		if(err) return fail(err,res)
+
+		console.log(body)
+	})
 });
 
 function onRequest(req,res){
