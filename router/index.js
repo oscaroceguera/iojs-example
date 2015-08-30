e2b2e9c6c222ad14583bd06888e1f3659d8a564c
oscar <oscaroceguera@gmail.com>
2015-08-29 23:54:58 -0600

    Router

---
+++ router/index.js
@@ -0,0 +1,18 @@
+const st = require('st');
+const path = require('path');
+
+const mount = st({
+	path : path.join(__dirname, '..', 'public'),
+	index : 'index.html'
+});
+
+function onRequest(req,res){
+	mount(req, res, function(err){
+		if (err) return res.end(err.message)
+
+		res.statusCode = 404;
+		res.end(`Not found ${req.url}`);
+	})
+}
+
+module.exports = onRequest;
--- server.js
+++ server.js
@@ -1,57 +1,18 @@
 'use strict'
 
 const http = require('http');
-const fs = require('fs'); // Cargar archivo
 const path = require('path'); // Manejo de rutas de archivos
+const router = require('./router');
+
 const port = process.env.PORT || 8080;
+
 
 const server = http.createServer();
 
-server.on('request', onRequest);
+server.on('request', router);
 server.on('listening', onListening)
 
 server.listen(port)
-
-function onRequest(req, res){
-	let uri = req.url;
-
-	if(uri.startsWith('/index') || uri === '/'){
-		return serveIndex(res);
-	}
-
-	if(uri === '/app.js'){
-		return serveApp(res)
-	}
-
-	res.statusCode = 404;
-	res.end(`404 not found: ${uri}`);
-}
-
-function serveIndex(res){
-	let index = path.join(__dirname, 'public', 'index.html');
-	let rs = fs.createReadStream(index);
-
-	res.setHeader('Content-Type', 'text/html');
-	rs.pipe(res);
-
-	rs.on('error', function(err){
-		res.setHeader('Content-Type', 'text/html');
-		res.end(err.message);
-	}) ;
-}
-
-function serveApp(res){
-	let app = path.join(__dirname, 'public', 'app.js');
-	let rs = fs.createReadStream(app);
-
-	res.setHeader('Content-Type', 'text/javascript');
-	rs.pipe(res);
-
-	rs.on('error', function(err){
-		res.setHeader('Content-Type', 'text/plain');
-		res.end(err.message);
-	}) ;
-}
 
 function onListening(){
 	console.log("servidor en escuchando en el puerto : " + port);
